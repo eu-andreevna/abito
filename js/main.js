e2337816721e6067cfd72047584ca19480320cfe
eu-andreevna <82045523+eu-andreevna@users.noreply.github.com>
2021-07-30 15:58:19 +0300

    Адаптировала под разные размеры экрана

--- js/main.js
+++ js/main.js
@@ -4,6 +4,26 @@ var swiper = new Swiper(".mySwiper", {
     freeMode: true,
     watchSlidesVisibility: true,
     watchSlidesProgress: true,
+    breakpoints: {
+        // when window width is >= 320px
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 50
+        },
+        // when window width is >= 640px
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        },
+        1200: {
+            slidesPerView: 4
+        }
+    }
 });
 var swiper2 = new Swiper(".mySwiper2", {
     spaceBetween: 10,
@@ -11,3 +31,14 @@ var swiper2 = new Swiper(".mySwiper2", {
         swiper: swiper,
     },
 });
+
+const menuBtn = document.querySelector('.menu-button');
+const menu = document.querySelector('.navbar-panel');
+const menuCloseBtn = document.querySelector('.close-menu');
+
+menuBtn.addEventListener("click", () => {
+    menu.classList.toggle("is-open");
+})
+menuCloseBtn.addEventListener("click", () => {
+    menu.classList.toggle("is-open");
+})
